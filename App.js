8119fce6514e7053dd39e76e935a9bdc4496e830
mrhorst <mhorst31@gmail.com>
2025-10-06 17:38:34 -0400

    finish 10.12. fix `package.json` not having `@apollo/client` and `graphql` as deps

--- App.js
+++ App.js
@@ -12,10 +12,12 @@ import { NativeRouter } from 'react-router-native';
 import { ApolloProvider } from '@apollo/client/react';
 import Main from './src/components/Main';
 import createApolloClient from './src/utils/apolloClient';
+import Constants from 'expo-constants';
 
 const client = createApolloClient();
 
 export default function App() {
+  console.log(Constants.expoConfig);
   return (
     <>
       <NativeRouter>
--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import Constants from 'expo-constants';
 
 const link = new HttpLink({
-  uri: 'http://localhost:4000/graphql',
+  uri: Constants.expoConfig.extra.APOLLO_URI,
 });
 
 const createApolloClient = () => {
